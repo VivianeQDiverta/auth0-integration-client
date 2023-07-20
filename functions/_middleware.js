dff4f5495eea62fd9d11cafd63b590bc11202612
Viviane Qian <viviane@diverta.co.jp>
2023-07-20 14:49:03 +0900

    add function to respond to post request

---
+++ functions/_middleware.js
@@ -0,0 +1,20 @@
+export async function onRequestPost() {
+  return new Response(null, {
+    headers: {
+      'Content-Type': 'text/html',
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
+}
+
+export async function onRequestOptions(context) {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': 'POST, OPTIONS',
+      'Access-Control-Max-Age': '86400',
+    },
+  });
+}
